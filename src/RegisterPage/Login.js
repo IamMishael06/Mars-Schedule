3cff7391396147a5a358a8e355d5a4f3865433bd
Tari Namikpoh <tarinamikpoh@gmail.com>
2024-04-09 01:21:01 +0100

    installed tailwind and started the register page

---
+++ src/RegisterPage/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+export default function Login() {
+  return (
+    <React.Fragment>
+       <div className='w-[100%] h-[100vh] overflow-hidden flex justify-center md:items-center md:bg-[#BB8A52]'>
+        <div className='w-[100%] h-[90%] relative bg-[#BB8A52] rounded-bl-[250px] flex flex-col justify-center relative'>
+           <div className='w-[90%] mx-auto h-[95%]'> 
+           <hr className='border-4 border-[#6D9773] w-[15%]' />
+           <form action=""></form>
+           <div>
+            
+           </div>
+           </div>
+        </div>
+       </div>
+    </React.Fragment>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/RegisterPage/Login.js"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+      'lato' :['Lato','sans-serif'],
+      'rubik':['Rubik','sans-serif'],
+      'quick' :['Quicksand','sans-serif']
+    },
+  },
+  plugins: [],
+}
+
