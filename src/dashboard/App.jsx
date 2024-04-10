2bb09e7e14452a068667b8eafc5e5c29b54ce72f
tolzzyyy <williamsegberongbe@gmail.com>
2024-04-10 12:02:57 +0100

    first commit

--- src/dashboard/App.jsx
+++ src/dashboard/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
 import { IoIosSearch } from "react-icons/io";
-import SideNav from './SideNav';
+
+import SideNav from "./SideNav";
 
 const App = () => {
   return (
-    <div className='bg-white w-full flex py-4 px-10 items-center mx-auto  h-screen'>
-      <div className='bg-[#BB8A52] flex w-full px-5 py-6 rounded-3xl h-full'>
-     <div className='w-1/12 flex justify-center pt-[60px]'><SideNav/></div>
-     <div className=' h-full w-11/12 py-4 bg-white rounded-2xl'>
-      <NavBar/>
-     </div>
-    </div>
-    </div>
-  )
-}
+    <div className="bg-white w-full flex py-4 px-10 items-center mx-auto  h-screen">
+      <div className="bg-[#BB8A52] flex w-full px-5 py-6 rounded-3xl h-full">
+        <div className="w-1/12 flex justify-center pt-[60px]">
+          <SideNav />
+        </div>
+        <div className=" h-full w-11/12 py-4 flex flex-col gap-4 bg-white rounded-2xl">
+          <NavBar />
 
-export default App
+     
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
